4198e33bfe9b2cdab7641cf6e2f305e44243dc4c
anselg <aesthapus@gmail.com>
2016-04-22 15:51:56 -0400

    grab vars[] array

--- make_readme.sh
+++ make_readme.sh
@@ -1,28 +1,30 @@
 #! /bin/bash
 
-CPPFILE=
-HEADER=
+CPPFILE=plugin-template.cpp
+HEADER=plugin-template.h
 README=NEW_README.md
 
-REQUIREMENTS=
-LIMITATIONS=
+REQUIREMENTS="none"
+LIMITATIONS="none"
 
-PLUGIN_NAME=$(cat "$CPPFILE" | sed -n "s/.*DefaultGUIModel(\"\(.*\)\".*/\1/p")
-DESCRIPTION=$(cat "$CPPFILE" | sed -n "s/.*setWhatsThis(\"\(.*\)\".*/\1/p")
+PLUGIN_NAME=$(cat "${CPPFILE}" | sed -n "s/.*DefaultGUIModel(\"\(.*\)\".*/\1/p")
+DESCRIPTION=$(cat "${CPPFILE}" | sed -n "s/.*setWhatsThis(\"\(.*\)\".*/\1/p")
+VARS_ARRAY=$(grep -Pzo "(?s)^(\s*)\N*vars\[\].*?{.*?^\1}" "${CPPFILE}")
 
-echo "### ${PLUGIN_NAME}" > "$README"
-echo "" >> "$README"
-echo "\*\*Requirements:\*\*\ ${REQUIREMENTS}" >> "$README"
-echo "\*\*Limitations:\*\*\ ${LIMITATIONS}" >> "$README"
-echo "" >> "$README"
-echo "<!--start-->" >> "$README"
-echo "${DESCRIPTION}" >> "$README"
-echo "<!--end-->" >> "$README"
-echo "" >> "$README"
-echo "#### Input" >> "$README"
-echo "" >> "$README"
-echo "#### Output" >> "$README"
-echo "" >> "$README"
-echo "#### Parameters" >> "$README"
-echo "" >> "$README"
-echo "#### States" >> "$README"
+
+echo "### ${PLUGIN_NAME}"                 > "${README}"
+echo ""                                  >> "${README}"
+echo "**Requirements:** ${REQUIREMENTS}" >> "${README}"
+echo "**Limitations:** ${LIMITATIONS}"   >> "${README}"
+echo ""                                  >> "${README}"
+echo "<!--start-->"                      >> "${README}"
+echo "${DESCRIPTION}"                    >> "${README}"
+echo "<!--end-->"                        >> "${README}"
+echo ""                                  >> "${README}"
+echo "#### Input"                        >> "${README}"
+echo ""                                  >> "${README}"
+echo "#### Output"                       >> "${README}"
+echo ""                                  >> "${README}"
+echo "#### Parameters"                   >> "${README}"
+echo ""                                  >> "${README}"
+echo "#### States"                       >> "${README}"

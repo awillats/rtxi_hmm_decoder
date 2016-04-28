8012049bbc5f92495861c7d33c4419d6ad5d0624
anselg <aesthapus@gmail.com>
2016-04-28 06:34:29 -0400

    add vars parsing and printing to readme generator

--- make_readme.sh
+++ make_readme.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-CPPFILE=plugin-template.cpp
-HEADER=plugin-template.h
-SCREENSHOT=plugin-template.png
-
 CPPFILE="${PWD##*/}.cpp"
 HEADER="${PWD##*/}.h"
 SCREENSHOT="${PWD##*/}.png"
@@ -15,25 +11,65 @@ LIMITATIONS="none"
 
 PLUGIN_NAME=$(cat "${CPPFILE}" | sed -n "s/.*DefaultGUIModel(\"\(.*\)\".*/\1/p")
 DESCRIPTION=$(cat "${CPPFILE}" | sed -n "s/.*setWhatsThis(\"\(.*\)\".*/\1/p")
-VARS_ARRAY=$(grep -Pzo "(?s)^(\s*)\N*vars\[\].*?{.*?^\1};" "${CPPFILE}" | \
-             grep -Pzo "(?s)^(\s*)\N.*?{.*?^\1}," | sed -e "s/\t//g" | \
-             sed ":a;N;$!ba;s/\n/ /g" | sed -e "s/DefaultGUIModel:://g")
+VARS_ARRAY=$(grep -Pzo "(?s)^(\s*)\N*vars\[\].*?{.*?^\1};" "${CPPFILE}")
+CATAPULT=$(echo ${VARS_ARRAY} | tr -d '\n' | \
+awk ' BEGIN { p=0 } 
+{
+   for(i=1; i<=length($0); i++)
+   {
+      test=substr($0, i, 1)
+      if(test=="{") {
+         p=1
+         continue
+      }
+      if(test=="}") {
+         printf("\n")
+         p=0;
+      }
+      if(p==1 && test!="\t") {
+         printf("%s", test)
+      }
+   }
+} ')
 
-echo "### ${PLUGIN_NAME}"                    > "${README}"
-echo ""                                     >> "${README}"
-echo "**Requirements:** ${REQUIREMENTS}"    >> "${README}"
-echo "**Limitations:** ${LIMITATIONS}"      >> "${README}"
-echo ""                                     >> "${README}"
-echo "![${PLUGIN_NAME} GUI](${SCREENSHOT})" >> "${README}"
-echo ""                                     >> "${README}"
-echo "<!--start-->"                         >> "${README}"
-echo "${DESCRIPTION}"                       >> "${README}"
-echo "<!--end-->"                           >> "${README}"
-echo ""                                     >> "${README}"
-echo "#### Input"                           >> "${README}"
-echo ""                                     >> "${README}"
-echo "#### Output"                          >> "${README}"
-echo ""                                     >> "${README}"
-echo "#### Parameters"                      >> "${README}"
-echo ""                                     >> "${README}"
-echo "#### States"                          >> "${README}"
+function PRINT_VARIABLES() {
+   LIST=$1
+   TYPE=$2
+   COUNTER=1
+   while read -r LINE; do
+      if [[ "$LINE" =~ "$TYPE" ]]; then
+         NAME=$(echo ${LINE} | cut -d "," -f1 | sed "s/^[ \t]*//" | sed -e "s/\"//g")
+         DESCRIPTION=$(echo ${LINE} | cut -d "," -f2 | sed "s/^[ \t]*//" | sed -e "s/\"//g")
+         if [[ "$TYPE" == INPUT ]] || [[ "$TYPE" == OUTPUT ]]; then
+            echo "$COUNTER. ${TYPE,,}($((COUNTER-1))) - $NAME : $DESCRIPTION"
+         else
+            echo "$COUNTER. $NAME - $DESCRIPTION"
+         fi
+         COUNTER=$((COUNTER+1))
+      fi
+   done <<< "${LIST}"
+}
+
+
+echo "### ${PLUGIN_NAME}"                          > "${README}"
+echo ""                                           >> "${README}"
+echo "**Requirements:** ${REQUIREMENTS}"          >> "${README}"
+echo "**Limitations:** ${LIMITATIONS}"            >> "${README}"
+echo ""                                           >> "${README}"
+echo "![${PLUGIN_NAME} GUI](${SCREENSHOT})"       >> "${README}"
+echo ""                                           >> "${README}"
+echo "<!--start-->"                               >> "${README}"
+echo "${DESCRIPTION}"                             >> "${README}"
+echo "<!--end-->"                                 >> "${README}"
+echo ""                                           >> "${README}"
+echo "#### Input"                                 >> "${README}"
+echo "$(PRINT_VARIABLES "${CATAPULT}" INPUT)"     >> "${README}"
+echo ""                                           >> "${README}"
+echo "#### Output"                                >> "${README}"
+echo "$(PRINT_VARIABLES "${CATAPULT}" OUTPUT)"    >> "${README}"
+echo ""                                           >> "${README}"
+echo "#### Parameters"                            >> "${README}"
+echo "$(PRINT_VARIABLES "${CATAPULT}" PARAMETER)" >> "${README}"
+echo ""                                           >> "${README}"
+echo "#### States"                                >> "${README}"
+echo "$(PRINT_VARIABLES "${CATAPULT}" STATE)"     >> "${README}"

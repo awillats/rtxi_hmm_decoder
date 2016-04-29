a355ee92a26c24f1fcb8d2983a3f7829d50427c2
anselg <aesthapus@gmail.com>
2016-04-29 14:05:57 -0400

    add comments to the build_readme.sh script

--- build_readme.sh
+++ build_readme.sh
@@ -1,5 +1,36 @@
 #! /bin/bash
 
+################################################################################
+# Copyright (C) 2011 Georgia Institute of Technology, University of Utah, 
+# Weill Cornell Medical College
+#
+# This program is free software: you can redistribute it and/or modify it under 
+# the terms of the GNU General Public License as published by the Free Software 
+# Foundation, either version 3 of the License, or (at your option) any later 
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT 
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+# details.
+# 
+# You should have received a copy of the GNU General Public License along with 
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+################################################################################
+# This script will build a README file using a module's source code, assuming 
+# the module abstracted from DefaultGUI. In the module directory, run: 
+# $ ./build_readme.sh
+################################################################################
+
+################################################################################
+# Set variables. Assume source code is files have the same name as the parent 
+# directory and the output file is called 'NEW_README.md'. 
+#
+# Manually override the default behaviour as needed. 
+################################################################################
+
 CPPFILE="${PWD##*/}.cpp"
 HEADER="${PWD##*/}.h"
 SCREENSHOT="${PWD##*/}.png"
@@ -52,6 +83,10 @@ function PRINT_VARIABLES() {
    done <<< "${LIST}"
 }
 
+################################################################################
+# Generate the README. The format is intended to be consistent with the pages 
+# found on http://rtxi.org/modules. 
+################################################################################
 
 echo "### ${PLUGIN_NAME}"                          > "${README}"
 echo ""                                           >> "${README}"

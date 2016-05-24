2c13d30f09ddfc5426e77c1e0c962eb1852e7805
Ansel <aesthapus@gmail.com>
2016-05-23 20:11:55 -0400

    add copyright info to rename_module.sh

--- rename_module.sh
+++ rename_module.sh
@@ -1,6 +1,24 @@
 # /bin/bash
 
 ################################################################################
+# Copyright (C) 2011 Georgia Institute of Technology, University of Utah, 
+# Weill Cornell Medical College
+#
+# This program is free software: you can redistribute it and/or modify it under 
+# the terms of the GNU General Public License as published by the Free Software 
+# Foundation, either version 3 of the License, or (at your option) any later 
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT 
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+# details.
+# 
+# You should have received a copy of the GNU General Public License along with 
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+################################################################################
 # Rename classes and filenames to correspond with the directory name. For 
 # example, if the directory is renamed my-favorite_module, the script will 
 # rename the PluginTemplate class to MyFavoriteModule and the source and header 

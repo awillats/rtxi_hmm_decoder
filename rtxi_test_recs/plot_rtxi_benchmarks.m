dc159e11966a36e09d8552fb91ee5f53ce3eedcf
awillats <awillats3@gatech.edu>
2019-04-12 14:49:24 -0400

    finalized formatting for displaying recordings

--- rtxi_test_recs/plot_rtxi_benchmarks.m
+++ rtxi_test_recs/plot_rtxi_benchmarks.m
@@ -18,38 +18,49 @@ XL = [0,2e4];
 base_time = 1e-9;
 %others: us = 1e-6; ms = 1e-3;
 time_unit = 'ms'; time_conv = base_time/1e-3;
-%time_unit = '\mus'; time_conv = base_time/1e-6; 
+time_unit = '\mus'; time_conv = base_time/1e-6; 
 
 
+compt = Data(4,:)*time_conv;
+compt1 = Data1(4,:)*time_conv;
+
+
+%f = @(x) log10(x); fi = @(y) 10.^y; ftxt1 = 'log10('; ftxt2 = ')';
+f = @(x) (x); fi = @(y) y; ftxt1 = ''; ftxt2 = '';
 
 
 figure(1)
 clf
-subplot(3,1,1)
+subplot(2,1,1)
 hold on
+plot(Data(1,:),'k')
+plot(Data(2,:),'g','LineWidth',2)
 plot(Data(3,:)-.1,'b','LineWidth',2)
+
 plot(Data(2,:),'g','LineWidth',2)
 plot(Data(1,:),'k')
 hold off
 
 xlim(XL);
+legend('spikes','true state','decoded state')
 
 
-subplot(3,1,2)
+subplot(2,1,2)
 hold on
 
-plot(log10(Data(4,:)*time_conv),'LineWidth',3) %Comp time in ns
-plot(log10(Data1(4,:)*time_conv),'LineWidth',3) %Comp time in ns
+plot(f(compt1),'LineWidth',3) %Comp time in ns
+plot(f(compt),'LineWidth',3) %Comp time in ns
 
 %plot(Data(5,:),'LineWidth',3) %peak RT period
-%ylim([0,200])
+ylim(f([0,2e2*1e3*time_conv]))
+legend('trial 1','trial 2')
 hold off
 xlim(XL);
 
 
-ylabel(['compute time [',time_unit,']'])
+ylabel(['compute time [',ftxt1,time_unit,ftxt2,']'])
 
-subplot(3,1,3)
+set(gcf,'Position',[         312         528        1006         270]);
 
 
 %xlim(XL);
@@ -57,21 +68,27 @@ subplot(3,1,3)
 
 figure(2)
 
-f = @(x) log10(x); ftxt1 = 'log10('; ftxt2 = ')';
-%f = @(x) (x);ftxt1 = ''; ftxt2 = '';
+
 
 clf
+
+[~,edges] = histcounts(log10(compt));
 hold on
-histogram(f(Data(4,:)*time_conv))
-histogram(f(Data1(4,:)*time_conv))
+histogram((compt), 10.^(edges))
+histogram((compt1), 10.^(edges))
 hold off
-xlabel(['compute time [',ftxt1,time_unit,ftxt2,']'])
+
+xlabel(['compute time [',time_unit,']'])
+
 %xlim([5,5.5]+log10(time_conv))
 
 
-xlim( f([1e5,power(10,5.5)]*time_conv))
+xlim( ([power(10,5),power(10,5.5)]*time_conv))
+title('Compute time distribution')
 
-set(gcf,'Position',[         291         600        1150         198]);
+set(gca,'XScale','log')
+
+set(gcf,'Position',[         312         240        1006         214]);
 
 
 
